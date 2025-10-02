a577d0dfdda992778fe2a897f5f38a7e43aee322
tma19993 <tmazzurek@gmail.com>
2025-10-02 18:30:40 +0200

    create dashboard panel

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { WaterIntake } from '../../core/services/water-intake';
+import { FormsModule } from '@angular/forms';
+import { FormatUnitsPipe } from '../../shared/pipes/format-units-pipe';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule, FormsModule,FormatUnitsPipe],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss'
 })
 export class Dashboard {
+  private waterIntakeService = inject(WaterIntake);
 
+  public intake$ = this.waterIntakeService.intake$;
+  public containers$ = this.waterIntakeService.containers$;
+
+  public amountToAdd = 0;
+  
+  public addManual() {
+    this.waterIntakeService.addManual(this.amountToAdd);
+    this.amountToAdd = 0;
+  }
+
+  public addContainer(containerId: string) {
+    this.waterIntakeService.addContainer(containerId);
+  }
+ 
 }
+
---
+++ src/app/shared/pipes/format-units-pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'formatUnits'
+})
+export class FormatUnitsPipe implements PipeTransform {
+
+  transform(value: number, ...args: unknown[]): unknown {
+    if (value >= 1000) return (value / 1000).toFixed(2) + ' L';
+    return value + ' ml';
+  }
+
+}
